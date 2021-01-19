797bcc019ed919462aa2606865d74b27392d70fd
Jennifer Huynh <jntlhuynh@gmail.com>
2021-01-19 00:28:32 -0800

    created constructor and method for changing read status

---
+++ script.js
@@ -0,0 +1,31 @@
+let myLibrary = ["Circe", "Clean Code", "Banapples"];
+
+function Book(author, title, numPages, read) {
+    this.author = author;
+    this.title = title;
+    this.numPages = numPages;
+    this.read = read;
+}
+
+Book.prototype.changeStatus = function() {
+    (this.read == true) ? this.read = false : this.read = true;
+}
+
+function addBookToLibrary() {
+    // To inherent an object use : Object.create(Book.prototype)
+    // To create an object : use new
+  // do stuff here
+  // function here to...
+  // take user’s input and store the new book objects into an array
+}
+
+// function to display books on their own card
+
+// New book button: author, title, #pages, read or not
+
+// Add delete button to each book's display
+
+// button to change books read status (toggle read status of book prototype instance)
+
+// add local storage
+// add cloud storage
