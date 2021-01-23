180947c58dd4601dfef0d8e79493f6ab595c864b
Jennifer Huynh <jntlhuynh@gmail.com>
2021-01-23 03:05:52 -0800

    Added modal code from TraversyMedia

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ Book.prototype.changeStatus = function() {
 }
 
 function getUserInput() {
-    
+
 }
 
 function addBookToLibrary() {
@@ -36,3 +36,30 @@ function addBookToLibrary() {
 
 // add local storage
 // add cloud storage
+
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
